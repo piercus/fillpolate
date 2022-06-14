f09fcc2f76538a6c777c116a5e4e94124f9c4c0a
Pierre Colle <piercus@gmail.com>
2022-06-14 15:26:23 +0200

    fix: add fillpolate config

--- README.md
+++ README.md
@@ -41,7 +41,7 @@ const obj2 = {
 };
 
 
-const results = fillpolate.run([obj1, obj2]);
+const results = fillpolate.expand([obj1, obj2]);
 
 console.log(results.length)
 /// 35, 36, 37, 38, 39 => 4
@@ -53,4 +53,52 @@ console.log(results[1]);
 // 	angleDeg: 1,
 // 	indexObj: 37
 // }
+
+const resultMiddle = fillpolate.extrapolateObject({
+	previous: obj1,
+	next: obj2,
+	weightPrevious: 0.5
+});
+
+
+```
+
+### Custom Usage
+
+```js
+
+const fillpolate = new Fillpolate({
+	indexKey: "indexObj",
+	schema: {
+		"type": "object",
+		"properties": {
+			"angleRad": {
+				"type": "integer",
+				"fillpolate": {
+					"strategy": "degree",
+					"to": (a) => a/Math.PI*180,
+					"from": (a) => a*Math.PI/180
+				}
+			}
+		}
+	}
+});
+
+const obj1 = {
+	angleRad: 90/180*Math.PI
+};
+
+
+const obj2 = {
+	angleRad: 180/180*Math.PI
+};
+
+const resultMiddle = fillpolate.extrapolateObject({
+	previous: obj1,
+	next: obj2,
+	weightPrevious: 0.5
+});
+
+console.log(resultMiddle.angleRad/Math.PI*180)
+// => 135
 ```
--- test/test.js
+++ test/test.js
@@ -29,7 +29,7 @@ test('simple scenario', t => {
 		indexObj: 39,
 	};
 
-	const results = fillpolate.run([object1, object2]);
+	const results = fillpolate.expand([object1, object2]);
 	t.is(results.length, 5);
 	t.is(results[2].num, 17);
 	t.is(results[2].angleDeg, 1);
@@ -37,4 +37,39 @@ test('simple scenario', t => {
 	t.is(results[2].fillpolated, true);
 
 	t.is(results[0].fillpolated, false);
+});
+
+test('custom scenario', t => {
+	const fillpolate = new Fillpolate({
+		indexKey: 'indexObj',
+		schema: {
+			type: 'object',
+			properties: {
+				angleRad: {
+					type: 'integer',
+					fillpolate: {
+						strategy: 'degree',
+						to: a => a / Math.PI * 180,
+						from: a => a * Math.PI / 180,
+					},
+				},
+			},
+		},
+	});
+
+	const object1 = {
+		angleRad: (90 + 360) / 180 * Math.PI,
+	};
+
+	const object2 = {
+		angleRad: 180 / 180 * Math.PI,
+	};
+
+	const resultMiddle = fillpolate.extrapolateObject({
+		previous: object1,
+		next: object2,
+		weightPrevious: 0.5,
+	});
+
+	t.is(resultMiddle.angleRad / Math.PI * 180, 135);
 });
